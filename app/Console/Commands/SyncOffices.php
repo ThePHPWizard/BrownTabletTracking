28928c42fb4190c8f584117c41c909d1df9f1a82
Unknown <steve@ninjagamehost.com>
2021-08-17 18:26:06 -0400

    Added Offices Script

---
+++ app/Console/Commands/SyncOffices.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Office;
+use Illuminate\Console\Command;
+
+class SyncOffices extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $offices = DB::connection('sqlsrv')->table('OmniTracs_TabletLocations')->get();
+        echo 'Found ' . count($offices) . ' offices!' . "\n";
+    
+        foreach ($offices as $office) {
+            echo 'Processing ' . $office->Tab_City . ', ' . $office->Tab_City . "\n";
+            $check = Tablet::where('city', $office->Tab_City)->where('state', $office->Tab_City)->first();
+            
+            if (empty($check)) {
+                $new_office = new Office();
+    
+                $new_office->city = $office->Tab_City;
+                $new_office->state = $office->Tab_City;
+    
+                $new_office->save();
+            
+            }
+        }
+        echo 'Complete ' . "\n";
+    }
+    }
+}
