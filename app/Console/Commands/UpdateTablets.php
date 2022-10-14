135d52b3f5159d10fb9600361fc974a29814983e
Unknown <steve@ninjagamehost.com>
2022-10-14 09:09:04 -0400

    Added debugging

--- app/Console/Commands/UpdateTablets.php
+++ app/Console/Commands/UpdateTablets.php
@@ -1,13 +1,13 @@
 <?php
-    
+
     namespace App\Console\Commands;
-    
+
     use App\Office;
     use App\Tablet;
     use Illuminate\Console\Command;
     use DB;
     use Log;
-    
+
     class UpdateTablets extends Command
     {
         /**
@@ -16,14 +16,14 @@
          * @var string
          */
         protected $signature = 'update:tablets';
-        
+
         /**
          * The console command description.
          *
          * @var string
          */
         protected $description = 'Command description';
-        
+
         /**
          * Create a new command instance.
          *
@@ -33,7 +33,7 @@
         {
             parent::__construct();
         }
-        
+
         /**
          * Execute the console command.
          *
@@ -43,14 +43,15 @@
         {
             $tablets = DB::connection('sqlsrv')->table('OmniTracs_TabletInventory')->get();
             echo 'Found ' . count($tablets) . ' tablets!' . "\n";
-            
+
             foreach ($tablets as $tablet) {
                 echo 'Processing ' . $tablet->Tab_Phone . "\n";
+                var_dump($tablet);
                 $imei = preg_replace('/[^0-9]/', '', $tablet->Tab_IMEI);
                 $mobile_number = preg_replace('/[^0-9]/', '', $tablet->Tab_Phone);
                 $check = Tablet::where('mobile_number', $mobile_number)->where('imei', $imei)->first();
                 if (empty($check)) {
-                    
+
                     $tablet_location = explode(' ', $tablet->Tab_Location);
                     if ($tablet->Tab_Status === 'In-Inventory') {
                         $status = 'In Inventory';
@@ -58,19 +59,19 @@
                         $status = $tablet->Tab_Status;
                     }
                     $location = Office::where('city', $tablet_location[0])->where('state', $tablet_location[1])->first();
-                    
-                    
+
+
                     $new_tablet = new Tablet();
-                    
+
                     $new_tablet->imei = $imei;
                     $new_tablet->mobile_number = $mobile_number;
                     $new_tablet->truck_id = null;
                     $new_tablet->status = $status;
                     $new_tablet->office_id = $location->id;
                     $new_tablet->created_by = 'System';
-                    
+
                     $new_tablet->save();
-                    
+
                 }
             }
             echo 'Complete ' . "\n";
